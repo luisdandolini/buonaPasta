c2ddcb4650b395f6e0d179c9a636531e258aa746
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-17 15:17:11 -0300

    add navbar

---
+++ buona-pasta/src/Navbar/index.js
@@ -0,0 +1,20 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return(
+    <header>
+      <a className='logo'>
+        Buona <span>Pasta</span>
+      </a>
+
+      <nav className='nav-links'>
+        <ul>
+          <li>Sobre</li>
+          <li>Produtos</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
